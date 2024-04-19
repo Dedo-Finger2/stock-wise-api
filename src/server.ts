21b86194657146881630e0e189f8db4789031ac9
Greg <antonioimportant@gmail.com>
2024-04-19 16:59:00 -0300

    chore: init

---
+++ src/server.ts
@@ -0,0 +1,6 @@
+import { env } from "./config/env";
+import { app } from "./config/app";
+
+app.listen({ port: env.PORT }).then(() => {
+  console.log(`Running... http://localhost:${env.PORT}/`);
+});
