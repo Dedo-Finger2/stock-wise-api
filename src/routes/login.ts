aff4f961a5c9208614cad9117ac54960eb90934f
Greg <antonioimportant@gmail.com>
2024-04-19 21:57:13 -0300

    feat: add login route

---
+++ src/routes/login.ts
@@ -0,0 +1,51 @@
+import { database } from "../config/database";
+import { FastifyInstance } from "fastify";
+import { MailrapMailService } from "../services/mailtrap-mail-service";
+import { z } from "zod";
+
+export async function login(app: FastifyInstance) {
+  app.post("/api/login", async (request, reply) => {
+    const loginBodySchema = z.object({
+      email: z.string().email()
+    });
+
+    const { email } = loginBodySchema.parse(request.body);
+
+    try {
+      const user = await database.user.findUnique({
+        where: {
+          email
+        }
+      });
+
+      if (user) {
+        const token = app.jwt.sign(
+          { userId: user.id },
+          {
+            expiresIn: "1h"
+          }
+        );
+
+        await new MailrapMailService().sendMessage({
+          to: {
+            name: user.name,
+            address: user.email
+          },
+          from: {
+            name: "Testing group",
+            address: "stockwisecompanty@stock.com"
+          },
+          subject: "Validation link",
+          html: `<a href='${token}'>Click here</a>`
+        });
+
+        return reply.status(200).send({ message: "Check your e-mail inbox." });
+      } else {
+        return reply.status(400).send({ message: "Email not registred. Try and register." });
+      }
+    } catch (error) {
+      console.error(error);
+      return reply.status(500).send({ message: "Internal Server Error." });
+    }
+  });
+}
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
 import { env } from "./config/env";
 import { app } from "./config/app";
 
+import { login } from "./routes/login";
+
+app.register(login);
+
 app.listen({ port: env.PORT }).then(() => {
   console.log(`Running... http://localhost:${env.PORT}/`);
 });
