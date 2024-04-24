6d8c40d0681cc51060345147e9982c82fb89449b
Greg <antonioimportant@gmail.com>
2024-04-24 11:05:57 -0300

    feat: add add-product to shopping list repository

---
+++ src/repositories/postgres/add-product-to-shopping-list.ts
@@ -0,0 +1,28 @@
+import { IRepository } from "../repository";
+import { database } from "../../config/database";
+import { Decimal } from "@prisma/client/runtime/library";
+
+interface ParamsSchema {
+  productId: string;
+  shoppingListId: string;
+}
+
+interface ReturnSchema {
+  productId: string;
+  shoppingListId: string;
+  quantityBought: number | null;
+  pricePaid: Decimal | null;
+}
+
+export class PostgresGetProductInShoppingListRepository implements IRepository {
+  async execute({ productId, shoppingListId }: ParamsSchema): Promise<ReturnSchema | null> {
+    const productAddedToShoppingList = await database.shoppingListProducts.create({
+      data: {
+        shoppingListId,
+        productId
+      }
+    });
+
+    return productAddedToShoppingList;
+  }
+}
